7731558747eb69a63f870060ab8a56286b81d2fd
Sarguna S <sargunasamidurai@gmail.com>
2024-08-03 15:40:58 +0530

    nextjs

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WpAAcFSIL9yMX3Xr7Ce+7tf3Ss19ktC+I9jAVtqg9QU=\"\n}"
---
+++ next.config.mjs
@@ -0,0 +1,4 @@
+/** @type {import('next').NextConfig} */
+
+const NextConfig={};
+export default NextConfig;
